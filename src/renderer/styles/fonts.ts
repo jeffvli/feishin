f09abdb4c62b9f207bc775b9992d22f813e127db
jeffvli <jeffvictorli@gmail.com>
2022-10-24 22:34:13 -0700

    Update styles

--- src/renderer/styles/fonts.ts
+++ src/renderer/styles/fonts.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import { css } from '@emotion/react';
 
 export enum Font {
   EPILOGUE = 'Epilogue',
--- src/renderer/styles/mixins.ts
+++ src/renderer/styles/mixins.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import { css } from '@emotion/react';
 
 export const textEllipsis = css`
   overflow: hidden;
