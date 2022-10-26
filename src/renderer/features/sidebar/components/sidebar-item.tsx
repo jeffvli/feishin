9204cdbe6a8ed7b22bda97dc42693f38011d360f
jeffvli <jeffvictorli@gmail.com>
2022-10-26 16:23:57 -0700

    Fix casing 2

---
+++ src/renderer/features/sidebar/components/sidebar-item.tsx
@@ -0,0 +1,71 @@
+import { ReactNode } from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Link, LinkProps } from 'react-router-dom';
+
+interface ListItemProps {
+  children: ReactNode;
+  disabled?: boolean;
+  to?: string;
+}
+
+const StyledItem = styled.div`
+  display: flex;
+  width: 100%;
+
+  &:focus-visible {
+    border: 1px solid var(--primary-color);
+  }
+`;
+
+const ItemStyle = css`
+  display: flex;
+  width: 100%;
+  padding: 0.5rem 1rem;
+  color: var(--sidebar-btn-color);
+  border: 1px transparent solid;
+  transition: color 0.2s ease-in-out;
+
+  &:hover {
+    color: var(--sidebar-btn-color-hover);
+  }
+`;
+
+const Box = styled.div`
+  ${ItemStyle}
+`;
+
+const ItemLink = styled(Link)<LinkProps & { disabled?: boolean }>`
+  opacity: ${(props) => props.disabled && 0.6};
+  pointer-events: ${(props) => props.disabled && 'none'};
+
+  &:focus-visible {
+    border: 1px solid var(--primary-color);
+  }
+
+  ${ItemStyle}
+`;
+
+export const SidebarItem = ({ to, children, ...rest }: ListItemProps) => {
+  if (to) {
+    return (
+      <ItemLink to={to} {...rest}>
+        {children}
+      </ItemLink>
+    );
+  }
+  return (
+    <StyledItem tabIndex={0} {...rest}>
+      {children}
+    </StyledItem>
+  );
+};
+
+SidebarItem.Box = Box;
+
+SidebarItem.Link = ItemLink;
+
+SidebarItem.defaultProps = {
+  disabled: false,
+  to: undefined,
+};
