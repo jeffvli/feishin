e9142ffaa554cccbe0f5205d799c630486386e91
jeffvli <jeffvictorli@gmail.com>
2022-11-03 03:17:27 -0700

    Add paper component

---
+++ src/renderer/components/paper/index.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import {
+  Paper as MantinePaper,
+  PaperProps as MantinePaperProps,
+} from '@mantine/core';
+
+export interface PaperProps extends MantinePaperProps {
+  children: React.ReactNode;
+}
+
+const StyledPaper = styled(MantinePaper)`
+  background: var(--paper-bg);
+`;
+
+export const Paper = ({ children, ...props }: PaperProps) => {
+  return <StyledPaper {...props}>{children}</StyledPaper>;
+};
