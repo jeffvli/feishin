f62aacf9d8529b69d828bf16378436c441fce76d
jeffvli <jeffvictorli@gmail.com>
2022-11-15 13:36:14 -0800

    Remove unused util

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -2,7 +2,6 @@ export * from './random-string';
 export * from './normalize-server-url';
 export * from './jellyfin';
 export * from './subsonic';
-export * from './server-folder-auth';
 export * from './set-local-storage-setttings';
 export * from './constrain-sidebar-width';
 export * from './title-case';
