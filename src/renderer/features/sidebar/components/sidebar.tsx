449e11c2d4dba817e04e773b75ca1903e2ac31ea
jeffvli <jeffvictorli@gmail.com>
2022-11-19 20:50:35 -0800

    Adjust object fit

--- src/renderer/features/sidebar/components/sidebar.tsx
+++ src/renderer/features/sidebar/components/sidebar.tsx
@@ -52,7 +52,7 @@ const ImageContainer = styled(motion(Link))<{ height: string }>`
 const SidebarImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   background: var(--placeholder-bg);
 `;
 
