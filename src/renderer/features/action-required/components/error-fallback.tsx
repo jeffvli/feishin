de1e6c16c0257cabea7f3a55047cdfee418dafa2
jeffvli <jeffvictorli@gmail.com>
2022-11-20 17:01:52 -0800

    Add logout to error fallback

--- src/renderer/features/action-required/components/error-fallback.tsx
+++ src/renderer/features/action-required/components/error-fallback.tsx
@@ -1,14 +1,18 @@
 import { Box, Center, Group, Stack } from '@mantine/core';
 import { FallbackProps } from 'react-error-boundary';
 import { RiErrorWarningLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { Button, Text } from '@/renderer/components';
+import { useAuthStore } from '@/renderer/store';
 
 const Container = styled(Box)`
   background: var(--main-bg);
 `;
 
 export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  const logout = useAuthStore((state) => state.logout);
+
   return (
     <Container>
       <Center sx={{ height: '100vh' }}>
@@ -20,6 +24,17 @@ export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
           <Text>{error.message}</Text>
           <Button variant="filled" onClick={resetErrorBoundary}>
             Reload
+          </Button>
+
+          <Button
+            component={Link}
+            to="/"
+            variant="filled"
+            onClick={() => {
+              logout();
+            }}
+          >
+            Log out
           </Button>
         </Stack>
       </Center>
