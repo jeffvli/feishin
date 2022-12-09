557eaab44c52adbf39b0a18a332df5d347c00c32
jeffvli <jeffvictorli@gmail.com>
2022-12-09 03:05:48 -0800

    Prevent initial animation on settings modal

--- packages/renderer/src/features/settings/components/settings.tsx
+++ packages/renderer/src/features/settings/components/settings.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mantine/core';
 import type { Variants } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { motion } from 'framer-motion';
 import { Tabs } from '/@/components';
 import { GeneralTab } from '/@/features/settings/components/general-tab';
@@ -29,42 +30,44 @@ export const Settings = () => {
       m={5}
       sx={{ height: '800px', maxHeight: '50vh', overflowX: 'hidden' }}
     >
-      <Tabs
-        keepMounted={false}
-        orientation="vertical"
-        styles={{
-          tab: {
-            fontSize: '1.1rem',
-            padding: '0.5rem 1rem',
-          },
-        }}
-        value={currentTab}
-        variant="pills"
-        onTabChange={(e) => e && update({ tab: e })}
-      >
-        <Tabs.List>
-          <Tabs.Tab value="general">General</Tabs.Tab>
-          <Tabs.Tab value="playback">Playback</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel value="general">
-          <motion.div
-            animate="in"
-            initial="out"
-            variants={tabVariants}
-          >
-            <GeneralTab />
-          </motion.div>
-        </Tabs.Panel>
-        <Tabs.Panel value="playback">
-          <motion.div
-            animate="in"
-            initial="out"
-            variants={tabVariants}
-          >
-            <PlaybackTab />
-          </motion.div>
-        </Tabs.Panel>
-      </Tabs>
+      <AnimatePresence initial={false}>
+        <Tabs
+          keepMounted={false}
+          orientation="vertical"
+          styles={{
+            tab: {
+              fontSize: '1.1rem',
+              padding: '0.5rem 1rem',
+            },
+          }}
+          value={currentTab}
+          variant="pills"
+          onTabChange={(e) => e && update({ tab: e })}
+        >
+          <Tabs.List>
+            <Tabs.Tab value="general">General</Tabs.Tab>
+            <Tabs.Tab value="playback">Playback</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="general">
+            <motion.div
+              animate="in"
+              initial="out"
+              variants={tabVariants}
+            >
+              <GeneralTab />
+            </motion.div>
+          </Tabs.Panel>
+          <Tabs.Panel value="playback">
+            <motion.div
+              animate="in"
+              initial="out"
+              variants={tabVariants}
+            >
+              <PlaybackTab />
+            </motion.div>
+          </Tabs.Panel>
+        </Tabs>
+      </AnimatePresence>
     </Box>
   );
 };
