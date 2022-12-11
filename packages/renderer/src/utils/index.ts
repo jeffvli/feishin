f6b8460eca29ced14fbd862c106e6752a621c75e
jeffvli <jeffvictorli@gmail.com>
2022-12-11 01:19:46 -0800

    Add header color util

--- packages/renderer/src/utils/index.ts
+++ packages/renderer/src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './normalize-server-url';
 export * from './set-local-storage-setttings';
 export * from './constrain-sidebar-width';
 export * from './title-case';
+export * from './get-header-color';
