9841fa3c637edafd48d8dfd80f070f26a6cd158f
jeffvli <jeffvictorli@gmail.com>
2022-12-26 05:15:19 -0800

    Use URLSearchParams for parser

--- src/renderer/utils/parse-search-params.ts
+++ src/renderer/utils/parse-search-params.ts
@@ -1,3 +1,17 @@
+import isUndefined from 'lodash/isUndefined';
+import omitBy from 'lodash/omitBy';
+
 export const parseSearchParams = (searchParams: Record<any, any>) => {
-  return JSON.parse(JSON.stringify(searchParams));
+  const params = new URLSearchParams();
+  const paramsWithoutUndefined = omitBy(searchParams, isUndefined);
+
+  Object.entries(paramsWithoutUndefined).forEach(([key, value]) => {
+    if (!Array.isArray(value)) {
+      params.append(key, value.toString());
+    } else {
+      value.forEach((value) => params.append(key, value.toString()));
+    }
+  });
+
+  return params.toString();
 };
