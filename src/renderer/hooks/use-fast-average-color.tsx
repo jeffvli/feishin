f2e6a418b01730bc8c91f3f635d3818791145660
jeffvli <jeffvictorli@gmail.com>
2023-01-03 02:28:59 -0800

    Add fallback to average color calculation

--- src/renderer/hooks/use-fast-average-color.tsx
+++ src/renderer/hooks/use-fast-average-color.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FastAverageColor } from 'fast-average-color';
 
 export const useFastAverageColor = (
   src?: string | null,
   aglorithm?: 'dominant' | 'simple' | 'sqrt',
 ) => {
+  const isMountedRef = useRef<boolean | null>(null);
   const [color, setColor] = useState<string | undefined>(undefined);
 
   useEffect(() => {
@@ -27,6 +28,10 @@ export const useFastAverageColor = (
           console.log('Error fetching average color', e);
           return setColor('rgba(0, 0, 0, 0)');
         });
+    } else if (isMountedRef.current) {
+      return setColor('var(--placeholder-bg)');
+    } else {
+      isMountedRef.current = true;
     }
 
     return () => {
