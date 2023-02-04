48a62fc2477668403e310d11b474a39cfbb1bb3e
jeffvli <jeffvictorli@gmail.com>
2023-02-03 19:05:58 -0800

    Add custom onClick callback to allow clearable rating

--- src/renderer/components/rating/index.tsx
+++ src/renderer/components/rating/index.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { MouseEvent } from 'react';
 import { Rating as MantineRating, RatingProps as MantineRatingProps } from '@mantine/core';
+import debounce from 'lodash/debounce';
 import styled from 'styled-components';
 
-type RatingProps = MantineRatingProps;
+interface RatingProps extends Omit<MantineRatingProps, 'onClick'> {
+  onClick: (e: MouseEvent<HTMLDivElement>, value: number | undefined) => void;
+}
 
 const StyledRating = styled(MantineRating)`
   & .mantine-Rating-symbolBody {
@@ -11,6 +16,13 @@ const StyledRating = styled(MantineRating)`
   }
 `;
 
-export const Rating = ({ ...props }: RatingProps) => {
-  return <StyledRating {...props} />;
+export const Rating = ({ onClick, ...props }: RatingProps) => {
+  const debouncedOnClick = debounce(onClick, 100);
+
+  return (
+    <StyledRating
+      {...props}
+      onClick={(e) => debouncedOnClick(e, props.value)}
+    />
+  );
 };
