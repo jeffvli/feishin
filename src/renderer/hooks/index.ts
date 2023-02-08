147b155d603b1ee6bb679361a751b9d0ef1f7f82
jeffvli <jeffvictorli@gmail.com>
2023-02-08 03:44:05 -0800

    Add hook for hideable scrollbar

--- src/renderer/hooks/index.ts
+++ src/renderer/hooks/index.ts
@@ -3,3 +3,4 @@ export * from './use-is-mounted';
 export * from './use-should-pad-titlebar';
 export * from './use-container-query';
 export * from './use-fast-average-color';
+export * from './use-hide-scrollbar';
