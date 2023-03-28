ececc394e2de50095a44321217b93b1768643084
jeffvli <jeffvictorli@gmail.com>
2023-03-28 16:13:18 -0700

    Fix filled button styles

--- src/renderer/features/shared/components/play-button.tsx
+++ src/renderer/features/shared/components/play-button.tsx
@@ -6,21 +6,21 @@ const MotionButton = styled(UnstyledButton)`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: var(--btn-primary-bg);
+  background-color: var(--btn-filled-bg);
   border: none;
   border-radius: 50%;
   opacity: 0.8;
 
   svg {
-    fill: var(--btn-primary-fg);
+    fill: var(--btn-filled-fg);
   }
 
   &:hover {
-    background-color: var(--btn-primary-bg-hover);
+    background-color: var(--btn-filled-bg-hover);
     transform: scale(1.1);
 
     svg {
-      fill: var(--btn-primary-fg-hover);
+      fill: var(--btn-filled-fg-hover);
     }
   }
 
