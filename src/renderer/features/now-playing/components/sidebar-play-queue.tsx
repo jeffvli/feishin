0c13b09029e1384b64511e04992b4bef73bb471a
jeffvli <jeffvictorli@gmail.com>
2023-03-29 20:39:59 -0700

    Fix window controls when sidebar queue enabled (#36)

--- src/renderer/features/now-playing/components/sidebar-play-queue.tsx
+++ src/renderer/features/now-playing/components/sidebar-play-queue.tsx
@@ -5,16 +5,24 @@ import { PlayQueue } from '/@/renderer/features/now-playing/components/play-queu
 import { PlayQueueListControls } from './play-queue-list-controls';
 import { Song } from '/@/renderer/api/types';
 import { PageHeader, Paper, VirtualGridContainer } from '/@/renderer/components';
+import { useGeneralSettings } from '/@/renderer/store/settings.store';
+import { Platform } from '/@/renderer/types';
 
 export const SidebarPlayQueue = () => {
   const queueRef = useRef<{ grid: AgGridReactType<Song> } | null>(null);
+  const { windowBarStyle } = useGeneralSettings();
 
   return (
     <VirtualGridContainer>
-      <Stack>
-        <PageHeader backgroundColor="var(--titlebar-bg)" />
-      </Stack>
-      <Paper>
+      {windowBarStyle === Platform.WEB && (
+        <Stack mr={windowBarStyle === Platform.WEB ? '130px' : undefined}>
+          <PageHeader backgroundColor="var(--titlebar-bg)" />
+        </Stack>
+      )}
+      <Paper
+        display={windowBarStyle !== Platform.WEB ? 'flex' : undefined}
+        h={windowBarStyle !== Platform.WEB ? '65px' : undefined}
+      >
         <PlayQueueListControls
           tableRef={queueRef}
           type="sideQueue"
