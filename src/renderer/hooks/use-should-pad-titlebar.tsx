b590636303e0ee253bb7a5bb790f3a54262b28a5
jeffvli <jeffvictorli@gmail.com>
2023-03-31 05:06:54 -0700

    Fix invalid import

--- src/renderer/hooks/use-should-pad-titlebar.tsx
+++ src/renderer/hooks/use-should-pad-titlebar.tsx
@@ -1,14 +1,14 @@
 import { useLocation } from 'react-router';
 import { AppRoute } from '/@/renderer/router/routes';
-import { useSidebarRightExpanded } from '/@/renderer/store';
-import { useGeneralSettings } from '/@/renderer/store/settings.store';
+import { useSidebarRightExpanded, useGeneralSettings, useWindowSettings } from '/@/renderer/store';
 import { Platform } from '/@/renderer/types';
 
 export const useShouldPadTitlebar = () => {
   const location = useLocation();
   const isSidebarExpanded = useSidebarRightExpanded();
   const isQueuePage = location.pathname === AppRoute.NOW_PLAYING;
-  const { sideQueueType, windowBarStyle } = useGeneralSettings();
+  const { sideQueueType } = useGeneralSettings();
+  const { windowBarStyle } = useWindowSettings();
 
   const conditions = [
     windowBarStyle === Platform.WEB,
