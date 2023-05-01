62670964c018914221013752df53c6e97277e730
jeffvli <jeffvictorli@gmail.com>
2023-04-30 22:05:06 -0700

    Add menu in error boundary

--- src/renderer/features/action-required/components/route-error-boundary.tsx
+++ src/renderer/features/action-required/components/route-error-boundary.tsx
@@ -1,7 +1,8 @@
 import { Center, Stack, Group, Divider, Box } from '@mantine/core';
-import { RiArrowLeftSLine, RiErrorWarningLine, RiHome4Line } from 'react-icons/ri';
+import { RiArrowLeftSLine, RiErrorWarningLine, RiHome4Line, RiMenuFill } from 'react-icons/ri';
 import { useNavigate, useRouteError } from 'react-router';
-import { Button, Text } from '/@/renderer/components';
+import { Button, DropdownMenu, Text } from '/@/renderer/components';
+import { AppMenu } from '/@/renderer/features/titlebar/components/app-menu';
 import { AppRoute } from '/@/renderer/router/routes';
 
 const RouteErrorBoundary = () => {
@@ -54,6 +55,23 @@ const RouteErrorBoundary = () => {
             >
               Go home
             </Button>
+            <DropdownMenu position="bottom-start">
+              <DropdownMenu.Target>
+                <Button
+                  leftIcon={<RiMenuFill />}
+                  size="md"
+                  sx={{ flex: 0.5 }}
+                  variant="default"
+                >
+                  Menu
+                </Button>
+              </DropdownMenu.Target>
+              <DropdownMenu.Dropdown>
+                <AppMenu />
+              </DropdownMenu.Dropdown>
+            </DropdownMenu>
+          </Group>
+          <Group grow>
             <Button
               size="md"
               variant="filled"
