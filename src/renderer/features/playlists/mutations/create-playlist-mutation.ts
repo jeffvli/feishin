3dfeed14327e1a7c522b883cf6714c1d2f11b6d0
jeffvli <jeffvictorli@gmail.com>
2023-05-08 03:35:51 -0700

    Invalidate playlist list on creation

--- src/renderer/features/playlists/mutations/create-playlist-mutation.ts
+++ src/renderer/features/playlists/mutations/create-playlist-mutation.ts
@@ -1,12 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { HTTPError } from 'ky';
 import { api } from '/@/renderer/api';
 import { CreatePlaylistArgs, CreatePlaylistResponse } from '/@/renderer/api/types';
 import { MutationHookArgs } from '/@/renderer/lib/react-query';
 import { getServerById } from '/@/renderer/store';
+import { queryKeys } from '../../../api/query-keys';
 
 export const useCreatePlaylist = (args: MutationHookArgs) => {
   const { options } = args || {};
+  const queryClient = useQueryClient();
 
   return useMutation<
     CreatePlaylistResponse,
@@ -18,6 +20,12 @@ export const useCreatePlaylist = (args: MutationHookArgs) => {
       const server = getServerById(args.serverId);
       if (!server) throw new Error('Server not found');
       return api.controller.createPlaylist({ ...args, apiClientProps: { server } });
+    },
+    onSuccess: (_args, variables) => {
+      const server = getServerById(variables.serverId);
+      if (server) {
+        queryClient.invalidateQueries(queryKeys.playlists.list(server.id));
+      }
     },
     ...options,
   });
