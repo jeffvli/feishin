b5da8aeb551ce0d01c721f0865d7da16a4e828c0
jeffvli <jeffvictorli@gmail.com>
2023-05-09 18:51:26 -0700

    Remove skeleton animation
    - Performance concerns due to large number of animated skeletons

--- src/renderer/components/skeleton/index.tsx
+++ src/renderer/components/skeleton/index.tsx
@@ -3,35 +3,8 @@ import { Skeleton as MantineSkeleton } from '@mantine/core';
 import styled from 'styled-components';
 
 const StyledSkeleton = styled(MantineSkeleton)`
-  @keyframes run {
-    0% {
-      left: 0;
-      transform: translateX(-100%);
-    }
-
-    80% {
-      transform: translateX(100%);
-    }
-
-    100% {
-      transform: translateX(100%);
-    }
-  }
-
-  &::before {
-    background: var(--skeleton-bg);
-  }
-
   &::after {
-    position: absolute;
-    background: linear-gradient(90deg, transparent, var(--skeleton-bg), transparent);
-    transform: translateX(-100%);
-    animation-name: run;
-    animation-duration: 1.5s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-    content: '';
-    inset: 0;
+    background: var(--placeholder-bg);
   }
 `;
 
