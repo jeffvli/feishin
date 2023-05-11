76ca03d8e39fb6c64f8586429933496865770c25
jeffvli <jeffvictorli@gmail.com>
2023-05-10 18:21:34 -0700

    Remove shadow on playerbar

--- src/renderer/layouts/default-layout/player-bar.tsx
+++ src/renderer/layouts/default-layout/player-bar.tsx
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 import { Playerbar } from '/@/renderer/features/player';
 
 const PlayerbarContainer = styled.footer`
-  z-index: 100;
+  z-index: 200;
   grid-area: player;
   background: var(--playerbar-bg);
-  filter: drop-shadow(0 -3px 1px rgba(0, 0, 0, 10%));
 `;
 
 export const PlayerBar = () => {
