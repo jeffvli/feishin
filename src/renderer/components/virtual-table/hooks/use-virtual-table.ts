c8663db4baae81cf875eaef544e9be8c2a8e3dcc
jeffvli <jeffvictorli@gmail.com>
2023-08-04 13:08:40 -0700

    Clean up

--- src/renderer/components/virtual-table/hooks/use-virtual-table.ts
+++ src/renderer/components/virtual-table/hooks/use-virtual-table.ts
@@ -166,8 +166,6 @@ export const useVirtualTable = <TFilter>({
                             },
                         });
 
-                        console.log('res', res);
-
                         return res;
                     })) as BasePaginatedResponse<any>;
 
