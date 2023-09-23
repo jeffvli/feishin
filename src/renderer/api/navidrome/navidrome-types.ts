ebcb7bc4d1bcf8ef77e675db1cca6d92c0abdafa
jeffvli <jeffvictorli@gmail.com>
2023-09-23 15:37:16 -0700

    Add path param for ND song list api

--- src/renderer/api/navidrome/navidrome-types.ts
+++ src/renderer/api/navidrome/navidrome-types.ts
@@ -259,6 +259,7 @@ const songListParameters = paginationParameters.extend({
     album_id: z.array(z.string()).optional(),
     artist_id: z.array(z.string()).optional(),
     genre_id: z.string().optional(),
+    path: z.string().optional(),
     starred: z.boolean().optional(),
     title: z.string().optional(),
     year: z.number().optional(),
