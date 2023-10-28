e6356398a11e6510a8279af3f73472eca5c41d37
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2023-10-28 16:58:47 -0700

    do not use tooltip

--- src/renderer/components/rating/index.tsx
+++ src/renderer/components/rating/index.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useCallback } from 'react';
 import { Rating as MantineRating, RatingProps } from '@mantine/core';
 import debounce from 'lodash/debounce';
 import styled from 'styled-components';
-import { Tooltip } from '/@/renderer/components/tooltip';
-import { useCallback } from 'react';
 
 const StyledRating = styled(MantineRating)`
     & .mantine-Rating-symbolBody {
@@ -30,21 +29,11 @@ export const Rating = ({ onChange, ...props }: RatingProps) => {
     const debouncedOnChange = debounce(valueChange, 100);
 
     return (
-        <Tooltip
-            label="Click on the same value to clear"
-            openDelay={1000}
-            onClick={(e) => {
-                // Why? For some reason, in prod, not having this results in scroll to top...
-                e.preventDefault();
-                e.stopPropagation();
+        <StyledRating
+            {...props}
+            onChange={(e) => {
+                debouncedOnChange(e);
             }}
-        >
-            <StyledRating
-                {...props}
-                onChange={(e) => {
-                    debouncedOnChange(e);
-                }}
-            />
-        </Tooltip>
+        />
     );
 };
