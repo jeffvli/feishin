0a658e3a220479dedd1bfb0fba9eae8dcb36ee12
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-02-09 23:20:01 +0000

    [bugfix]: default go libsecret, support changing secret store (#493)
    * [bugfix]: default go libsecret, support changing secret store
    
    * update readme and rename libsecret

--- src/renderer/features/settings/components/window/window-tab.tsx
+++ src/renderer/features/settings/components/window/window-tab.tsx
@@ -2,6 +2,10 @@ import { Divider, Stack } from '@mantine/core';
 import { UpdateSettings } from '/@/renderer/features/settings/components/window/update-settings';
 import { WindowSettings } from '/@/renderer/features/settings/components/window/window-settings';
 import { DiscordSettings } from '/@/renderer/features/settings/components/window/discord-settings';
+import isElectron from 'is-electron';
+import { PasswordSettings } from '/@/renderer/features/settings/components/window/password-settings';
+
+const utils = isElectron() ? window.electron.utils : null;
 
 export const WindowTab = () => {
     return (
@@ -11,6 +15,12 @@ export const WindowTab = () => {
             <DiscordSettings />
             <Divider />
             <UpdateSettings />
+            {utils?.isLinux() && (
+                <>
+                    <Divider />
+                    <PasswordSettings />
+                </>
+            )}
         </Stack>
     );
 };
