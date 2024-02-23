9b760b0ca475938f1a9a88219b3a24e3825c8c18
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-02-22 23:02:43 -0800

    show window when dock clicked macos

--- src/main/main.ts
+++ src/main/main.ts
@@ -610,6 +610,10 @@ if (!singleInstance) {
                 // On macOS it's common to re-create a window in the app when the
                 // dock icon is clicked and there are no other windows open.
                 if (mainWindow === null) createWindow(false);
+                else if (!mainWindow.isVisible()) {
+                    mainWindow.show();
+                    createWinThumbarButtons();
+                }
             });
         })
         .catch(console.log);
