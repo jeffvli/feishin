abdb2fee857e5c7352b4c8adf19ebb17af892934
Dylan Lathrum <dylanlathrum@gmail.com>
2024-04-24 22:43:28 -0700

    Allow smaller album covers in card/poster display

--- src/renderer/features/albums/components/album-list-header-filters.tsx
+++ src/renderer/features/albums/components/album-list-header-filters.tsx
@@ -545,7 +545,7 @@ export const AlbumListHeaderFilters = ({ gridRef, tableRef }: AlbumListHeaderFil
                             <Slider
                                 defaultValue={isGrid ? grid?.itemSize || 0 : table.rowHeight}
                                 max={isGrid ? 300 : 100}
-                                min={isGrid ? 150 : 25}
+                                min={isGrid ? 100 : 25}
                                 onChangeEnd={handleItemSize}
                             />
                         </DropdownMenu.Item>
