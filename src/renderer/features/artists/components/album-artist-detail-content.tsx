1be9673a764ba91ca0ed6b285d9e5eb6a0761a09
sel10ut <45976019+sel10ut@users.noreply.github.com>
2024-05-27 10:46:45 +0300

    tweak(jellyfin): fetch actual recent album releases

--- src/renderer/features/artists/components/album-artist-detail-content.tsx
+++ src/renderer/features/artists/components/album-artist-detail-content.tsx
@@ -99,7 +99,7 @@ export const AlbumArtistDetailContent = ({ background }: AlbumArtistDetailConten
             _custom: {
                 jellyfin: {
                     ...(server?.type === ServerType.JELLYFIN
-                        ? { ArtistIds: albumArtistId }
+                        ? { AlbumArtistIds: albumArtistId }
                         : undefined),
                 },
                 navidrome: {
