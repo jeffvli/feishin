d9f19202faf5097e86b4db145836a6ac6920f99a
0PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-08-17 15:16:26 +0200

    refactor: adjust audio frecuencies

--- src/renderer/utils/audio-info.ts
+++ src/renderer/utils/audio-info.ts
@@ -9,7 +9,7 @@ export type AudioBand = {
 };
 
 // Bands from https://ia601608.us.archive.org/27/items/gov.law.ansi.s1.11.2004/ansi.s1.11.2004.pdf#page=23
-export const AudioFrequencies = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 20000];
+export const AudioFrequencies = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
 
 const OctaveToQ = {
     [Octave.Full]: 1.414214,
