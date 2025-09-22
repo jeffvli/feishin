953f07faa8f1c8065862a5dd9b0beacf7d023167
jeffvli <jeffvictorli@gmail.com>
2025-09-21 20:22:55 -0700

    add breakpoints postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,16 @@
 module.exports = {
     plugins: {
         'postcss-preset-mantine': {},
+        'postcss-simple-vars': {
+            variables: {
+                'breakpoint-xs': '36em',
+                'breakpoint-sm': '48em',
+                'breakpoint-md': '62em',
+                'breakpoint-lg': '75em',
+                'breakpoint-xl': '88em',
+                'breakpoint-2xl': '120em',
+                'breakpoint-3xl': '160em',
+            },
+        },
     },
 };
