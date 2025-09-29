1bbbe569013d4aed0e4f50940338dc8d239c4d1d
jeffvli <jeffvictorli@gmail.com>
2025-09-29 00:34:08 -0700

    add paginated album grid component

---
+++ src/renderer/features/albums/components/album-list-paginated-grid.tsx
@@ -0,0 +1,65 @@
+import { UseSuspenseQueryOptions } from '@tanstack/react-query';
+import { forwardRef } from 'react';
+
+import { api } from '/@/renderer/api';
+import {
+    PaginatedListProps,
+    useItemListPaginatedLoader,
+} from '/@/renderer/components/item-list/helpers/item-list-paginated-loader';
+import { ItemGrid } from '/@/renderer/components/item-list/item-grid/item-grid';
+import { ItemListWithPagination } from '/@/renderer/components/item-list/item-list-pagination/item-list-pagination';
+import { useItemListPagination } from '/@/renderer/components/item-list/item-list-pagination/use-item-list-pagination';
+import { albumQueries } from '/@/renderer/features/albums/api/album-api';
+import {
+    AlbumListQuery,
+    AlbumListSort,
+    LibraryItem,
+    SortOrder,
+} from '/@/shared/types/domain-types';
+
+interface AlbumListPaginatedGridProps extends PaginatedListProps<AlbumListQuery> {}
+
+export const AlbumListPaginatedGrid = forwardRef<any, AlbumListPaginatedGridProps>(
+    (
+        {
+            initialPage,
+            itemsPerPage = 100,
+            query = {
+                sortBy: AlbumListSort.NAME,
+                sortOrder: SortOrder.ASC,
+            },
+            serverId,
+        },
+        ref,
+    ) => {
+        const listCountQuery = albumQueries.listCount({
+            query: { ...query },
+            serverId: serverId,
+        }) as UseSuspenseQueryOptions<number, Error, number, readonly unknown[]>;
+
+        const listQueryFn = api.controller.getAlbumList;
+
+        const { currentPage, onChange } = useItemListPagination({ initialPage });
+
+        const { data, pageCount, totalItemCount } = useItemListPaginatedLoader({
+            currentPage,
+            itemsPerPage,
+            listCountQuery,
+            listQueryFn,
+            query,
+            serverId,
+        });
+
+        return (
+            <ItemListWithPagination
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+                onChange={onChange}
+                pageCount={pageCount}
+                totalItemCount={totalItemCount}
+            >
+                <ItemGrid data={data || []} itemType={LibraryItem.ALBUM} ref={ref} />
+            </ItemListWithPagination>
+        );
+    },
+);
