5f79229970dc3996750df8f1c7d6bb1dfed9c113
jeffvli <jeffvictorli@gmail.com>
2025-09-28 20:38:32 -0700

    finalize infinite album list

--- src/renderer/features/albums/components/album-list-infinite-grid.tsx
+++ src/renderer/features/albums/components/album-list-infinite-grid.tsx
@@ -1,34 +1,45 @@
-import { UseQueryOptions, UseSuspenseQueryOptions } from '@tanstack/react-query';
+import { UseSuspenseQueryOptions } from '@tanstack/react-query';
 import { forwardRef } from 'react';
 
+import { api } from '/@/renderer/api';
 import {
     InfiniteListProps,
     useItemListInfiniteLoader,
 } from '/@/renderer/components/item-list/helpers/item-list-loader';
 import { ItemGrid } from '/@/renderer/components/item-list/item-grid/item-grid';
 import { albumQueries } from '/@/renderer/features/albums/api/album-api';
-import { AlbumListQuery, AlbumListResponse, LibraryItem } from '/@/shared/types/domain-types';
+import {
+    AlbumListQuery,
+    AlbumListSort,
+    LibraryItem,
+    SortOrder,
+} from '/@/shared/types/domain-types';
 
 interface AlbumListInfiniteGridProps extends InfiniteListProps<AlbumListQuery> {}
 
 export const AlbumListInfiniteGrid = forwardRef<any, AlbumListInfiniteGridProps>(
-    ({ query, serverId }, ref) => {
+    (
+        {
+            query = {
+                sortBy: AlbumListSort.NAME,
+                sortOrder: SortOrder.ASC,
+            },
+            serverId,
+        },
+        ref,
+    ) => {
         const listCountQuery = albumQueries.listCount({
             query: { ...query },
             serverId: serverId,
         }) as UseSuspenseQueryOptions<number, Error, number, readonly unknown[]>;
 
-        const listQuery = albumQueries.list({
-            query: { ...query, startIndex: 0 },
-            serverId: serverId,
-        }) as UseQueryOptions<AlbumListResponse, Error, AlbumListResponse, readonly unknown[]>;
+        const listQueryFn = api.controller.getAlbumList;
 
         const { data, onRangeChanged } = useItemListInfiniteLoader({
             itemsPerPage: 100,
-            itemType: LibraryItem.ALBUM,
             listCountQuery,
-            listQuery,
-            params: query,
+            listQueryFn,
+            query,
             serverId,
         });
 
