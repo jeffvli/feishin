23005ba857d200deef7e4010f33c443721f1f403
jeffvli <jeffvictorli@gmail.com>
2025-10-08 21:38:16 -0700

    forward switch ref

--- src/shared/components/switch/switch.tsx
+++ src/shared/components/switch/switch.tsx
@@ -1,23 +1,27 @@
 import type { SwitchProps as MantineSwitchProps } from '@mantine/core';
 
 import { Switch as MantineSwitch } from '@mantine/core';
+import { forwardRef, Ref } from 'react';
 
 import styles from './switch.module.css';
 
 type SwitchProps = MantineSwitchProps;
 
-export const Switch = ({ classNames, ...props }: SwitchProps) => {
-    return (
-        <MantineSwitch
-            classNames={{
-                input: styles.input,
-                root: styles.root,
-                thumb: styles.thumb,
-                track: styles.track,
-                ...classNames,
-            }}
-            withThumbIndicator={false}
-            {...props}
-        />
-    );
-};
+export const Switch = forwardRef(
+    ({ classNames, ...props }: SwitchProps, ref: Ref<HTMLInputElement>) => {
+        return (
+            <MantineSwitch
+                classNames={{
+                    input: styles.input,
+                    root: styles.root,
+                    thumb: styles.thumb,
+                    track: styles.track,
+                    ...classNames,
+                }}
+                ref={ref}
+                withThumbIndicator={false}
+                {...props}
+            />
+        );
+    },
+);
