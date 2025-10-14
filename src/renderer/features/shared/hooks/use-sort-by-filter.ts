3a01ee756e8dd88d76fafc98b0503f5600415988
jeffvli <jeffvictorli@gmail.com>
2025-10-13 18:31:47 -0700

    enforce default value on sortBy filter

--- src/renderer/features/shared/hooks/use-sort-by-filter.ts
+++ src/renderer/features/shared/hooks/use-sort-by-filter.ts
@@ -2,14 +2,14 @@ import { parseAsString, useQueryState } from 'nuqs';
 
 import { FILTER_KEYS } from '/@/renderer/features/shared/utils';
 
-export const useSortByFilter = <TSortBy>(defaultValue?: string) => {
+export const useSortByFilter = <TSortBy>(defaultValue: string) => {
     const [sortBy, setSortBy] = useQueryState(
         FILTER_KEYS.SHARED.SORT_BY,
         defaultValue ? parseAsString.withDefault(defaultValue) : parseAsString,
     );
 
     return {
-        [FILTER_KEYS.SHARED.SORT_BY]: (sortBy as TSortBy) ?? undefined,
+        [FILTER_KEYS.SHARED.SORT_BY]: sortBy as TSortBy,
         setSortBy,
     };
 };
