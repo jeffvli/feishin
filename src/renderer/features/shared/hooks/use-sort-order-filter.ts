6ceb10534b1e4cc3c5213d3a1db690797371cced
jeffvli <jeffvictorli@gmail.com>
2025-10-13 18:32:12 -0700

    enforce default value on sortOrder filter

--- src/renderer/features/shared/hooks/use-sort-order-filter.ts
+++ src/renderer/features/shared/hooks/use-sort-order-filter.ts
@@ -3,14 +3,14 @@ import { parseAsString, useQueryState } from 'nuqs';
 import { FILTER_KEYS } from '/@/renderer/features/shared/utils';
 import { SortOrder } from '/@/shared/types/domain-types';
 
-export const useSortOrderFilter = (defaultValue?: string) => {
+export const useSortOrderFilter = (defaultValue: string) => {
     const [sortOrder, setSortOrder] = useQueryState(
         FILTER_KEYS.SHARED.SORT_ORDER,
         defaultValue ? parseAsString.withDefault(defaultValue) : parseAsString,
     );
 
     return {
-        [FILTER_KEYS.SHARED.SORT_ORDER]: (sortOrder as SortOrder) ?? undefined,
+        [FILTER_KEYS.SHARED.SORT_ORDER]: sortOrder as SortOrder,
         setSortOrder,
     };
 };
