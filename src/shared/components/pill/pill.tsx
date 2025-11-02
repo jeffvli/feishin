cf35fbd92d5ef8e8437b2244960239a1cdadf217
jeffvli <jeffvictorli@gmail.com>
2025-11-01 19:16:07 -0700

    set pill default size to md

--- src/shared/components/pill/pill.tsx
+++ src/shared/components/pill/pill.tsx
@@ -1,7 +1,11 @@
 import { Pill as MantinePill, PillProps as MantinePillProps } from '@mantine/core';
 
 export const Pill = ({ children, ...props }: MantinePillProps) => {
-    return <MantinePill {...props}>{children}</MantinePill>;
+    return (
+        <MantinePill size="md" {...props}>
+            {children}
+        </MantinePill>
+    );
 };
 
 Pill.Group = MantinePill.Group;
