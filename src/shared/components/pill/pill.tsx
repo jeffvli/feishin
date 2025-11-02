d0148572e07eee34a20625727430e39e7ec1c381
jeffvli <jeffvictorli@gmail.com>
2025-11-01 20:02:17 -0700

    standardize pill styling

--- src/shared/components/pill/pill.tsx
+++ src/shared/components/pill/pill.tsx
@@ -1,8 +1,26 @@
 import { Pill as MantinePill, PillProps as MantinePillProps } from '@mantine/core';
+import clsx from 'clsx';
 
-export const Pill = ({ children, ...props }: MantinePillProps) => {
+import styles from './pill.module.css';
+
+export const Pill = ({ children, size = 'md', ...props }: MantinePillProps) => {
     return (
-        <MantinePill size="md" {...props}>
+        <MantinePill
+            classNames={{
+                label: clsx({
+                    [styles.label]: true,
+                    [styles.lg]: size === 'lg',
+                    [styles.md]: size === 'md',
+                    [styles.sm]: size === 'sm',
+                    [styles.xl]: size === 'xl',
+                    [styles.xs]: size === 'xs',
+                }),
+                remove: styles.remove,
+                root: styles.root,
+            }}
+            size="md"
+            {...props}
+        >
             {children}
         </MantinePill>
     );
