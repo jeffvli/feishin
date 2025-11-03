7859e45a302821629a371185d217ae18dd21b53e
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-11-02 22:03:50 -0800

    standardize json keys

--- src/renderer/utils/normalize-release-types.tsx
+++ src/renderer/utils/normalize-release-types.tsx
@@ -1,7 +1,30 @@
 import { TFunction } from 'react-i18next';
 
-import en from '/@/i18n/locales/en.json';
 import { titleCase } from '/@/renderer/utils/title-case';
+
+// Release types derived from https://musicbrainz.org/doc/Release_Group/Type
+const PRIMARY_MAPPING = {
+    album: 'album',
+    broadcast: 'broadcast',
+    ep: 'ep',
+    other: 'other',
+    single: 'single',
+} as const;
+
+const SECONDARY_MAPPING = {
+    audiobook: 'audiobook',
+    'audio drama': 'audioDrama',
+    compilation: 'compilation',
+    demo: 'demo',
+    'dj-mix': 'djMix',
+    'field recording': 'fieldRecording',
+    interview: 'interview',
+    live: 'live',
+    'mixtape/street': 'mixtape',
+    remix: 'remix',
+    soundtrack: 'soundtrack',
+    spokenword: 'spokenWord',
+} as const;
 
 export const normalizeReleaseTypes = (types: string[], t: TFunction) => {
     const primary: string[] = [];
@@ -11,10 +34,16 @@ export const normalizeReleaseTypes = (types: string[], t: TFunction) => {
     for (const type of types) {
         const lower = type.toLocaleLowerCase();
 
-        if (lower in en.releaseType.primary) {
-            primary.push(t(`releaseType.primary.${lower}`, { postProcess: 'sentenceCase' }));
-        } else if (lower in en.releaseType.secondary) {
-            secondary.push(t(`releaseType.secondary.${lower}`, { postProcess: 'sentenceCase' }));
+        if (lower in PRIMARY_MAPPING) {
+            primary.push(
+                t(`releaseType.primary.${PRIMARY_MAPPING[lower]}`, { postProcess: 'sentenceCase' }),
+            );
+        } else if (lower in SECONDARY_MAPPING) {
+            secondary.push(
+                t(`releaseType.secondary.${SECONDARY_MAPPING[lower]}`, {
+                    postProcess: 'sentenceCase',
+                }),
+            );
         } else {
             unknown.push(titleCase(type));
         }
