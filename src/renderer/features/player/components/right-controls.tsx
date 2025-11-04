2a2d624a8f603d2d3b97d378af9b44174456133e
Flleeppyy <flleeppyy@gmail.com>
2025-11-04 00:23:50 -0800

    feat: add 'preservePitch' toggle to playback speed "dropdown"

--- src/renderer/features/player/components/right-controls.tsx
+++ src/renderer/features/player/components/right-controls.tsx
@@ -14,8 +14,10 @@ import {
     useCurrentSong,
     useHotkeySettings,
     useMuted,
+    usePlaybackSettings,
     usePreviousSong,
     useSettingsStore,
+    useSettingsStoreActions,
     useSidebarStore,
     useSpeed,
     useVolume,
@@ -24,8 +26,10 @@ import { ActionIcon } from '/@/shared/components/action-icon/action-icon';
 import { DropdownMenu } from '/@/shared/components/dropdown-menu/dropdown-menu';
 import { Flex } from '/@/shared/components/flex/flex';
 import { Group } from '/@/shared/components/group/group';
+import { Option } from '/@/shared/components/option/option';
 import { Rating } from '/@/shared/components/rating/rating';
 import { Slider } from '/@/shared/components/slider/slider';
+import { Switch } from '/@/shared/components/switch/switch';
 import { LibraryItem, QueueSong, ServerType, Song } from '/@/shared/types/domain-types';
 
 const ipc = isElectron() ? window.api.ipc : null;
@@ -50,9 +54,12 @@ export const RightControls = () => {
         handleVolumeUp,
         handleVolumeWheel,
     } = useRightControls();
+    const { setSettings } = useSettingsStoreActions();
+    const playbackSettings = usePlaybackSettings();
 
     const speed = useSpeed();
     const volumeWidth = useSettingsStore((state) => state.general.volumeWidth);
+    const speedPreservePitch = useSettingsStore((state) => state.playback.preservePitch);
 
     const updateRatingMutation = useSetRating({});
     const addToFavoritesMutation = useCreateFavorite({});
@@ -227,6 +234,26 @@ export const RightControls = () => {
                         />
                     </DropdownMenu.Target>
                     <DropdownMenu.Dropdown>
+                        <Option>
+                            <Option.Label>
+                                {t('setting.preservePitch', {
+                                    postProcess: 'sentenceCase',
+                                })}
+                            </Option.Label>
+                            <Option.Control>
+                                <Switch
+                                    defaultChecked={speedPreservePitch}
+                                    onChange={(e) => {
+                                        setSettings({
+                                            playback: {
+                                                ...playbackSettings,
+                                                preservePitch: e.currentTarget.checked,
+                                            },
+                                        });
+                                    }}
+                                />
+                            </Option.Control>
+                        </Option>
                         <Slider
                             label={formatPlaybackSpeedSliderLabel}
                             marks={[
