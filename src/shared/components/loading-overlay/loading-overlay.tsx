c5fb5da574c22626d53a0bf696bb892fc8ff2a7d
jeffvli <jeffvictorli@gmail.com>
2025-11-10 20:27:13 -0800

    add LoadingOverlay component

---
+++ src/shared/components/loading-overlay/loading-overlay.tsx
@@ -0,0 +1,28 @@
+import {
+    LoadingOverlay as MantineLoadingOverlay,
+    LoadingOverlayProps as MantineLoadingOverlayProps,
+} from '@mantine/core';
+
+import { Spinner } from '/@/shared/components/spinner/spinner';
+
+interface LoadingOverlayProps extends MantineLoadingOverlayProps {
+    color?: string;
+    opacity?: number;
+}
+
+export const LoadingOverlay = ({ ...props }: LoadingOverlayProps) => {
+    return (
+        <MantineLoadingOverlay
+            loaderProps={{ children: <Spinner /> }}
+            overlayProps={{
+                color: 'var(--theme-colors-background)',
+                opacity: 0.5,
+            }}
+            transitionProps={{
+                duration: 0.5,
+                transition: 'fade',
+            }}
+            {...props}
+        />
+    );
+};
