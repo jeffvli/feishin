fb967e3ccf36684990ec3a2cb30992aeaffb3440
jeffvli <jeffvictorli@gmail.com>
2025-11-13 09:24:26 -0800

    add indexeddb storage for zustand

--- src/renderer/store/utils.ts
+++ src/renderer/store/utils.ts
@@ -1,5 +1,6 @@
+import { del, get, set } from 'idb-keyval';
 import mergeWith from 'lodash/mergeWith';
-
+import { StateStorage } from 'zustand/middleware';
 /**
  * A custom deep merger that will replace all 'columns' items with the persistent
  * state, instead of the default merge behavior. This is important to preserve the user's
@@ -16,4 +17,19 @@ export const mergeOverridingColumns = <T>(persistedState: unknown, currentState:
 
         return undefined;
     });
+};
+
+export const idbStateStorage: StateStorage = {
+    getItem: async (name: string): Promise<null | string> => {
+        console.log(name, 'has been retrieved');
+        return (await get(name)) || null;
+    },
+    removeItem: async (name: string): Promise<void> => {
+        console.log(name, 'has been deleted');
+        await del(name);
+    },
+    setItem: async (name: string, value: string): Promise<void> => {
+        console.log(name, 'with value', value, 'has been saved');
+        await set(name, value);
+    },
 };
