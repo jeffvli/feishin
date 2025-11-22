2ae34d1b6678683f44b2d2d2daa1bc9d98713ab0
jeffvli <jeffvictorli@gmail.com>
2025-11-21 19:03:07 -0800

    remove most built-in fonts, adjust NotoSans weight

--- src/renderer/types/fonts.ts
+++ src/renderer/types/fonts.ts
@@ -6,15 +6,8 @@ export type Font = {
 };
 
 export const FONT_OPTIONS: Font[] = [
-    { label: 'Archivo', value: 'Archivo' },
-    { label: 'Fredoka', value: 'Fredoka' },
     { label: 'Inter', value: 'Inter' },
-    { label: 'League Spartan', value: 'League Spartan' },
-    { label: 'Lexend', value: 'Lexend' },
     { label: 'Poppins', value: 'Poppins' },
-    { label: 'Raleway', value: 'Raleway' },
-    { label: 'Sora', value: 'Sora' },
-    { label: 'Work Sans', value: 'Work Sans' },
 ];
 
 export const FontValueSchema = z.enum(
