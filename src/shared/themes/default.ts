a3eb6604414606cc80324ef45de3390703fb675a
jeffvli <jeffvictorli@gmail.com>
2025-11-22 14:37:52 -0800

    increase default content max-width to 1800px

--- src/shared/themes/default.ts
+++ src/shared/themes/default.ts
@@ -2,7 +2,7 @@ import { AppThemeConfiguration } from './app-theme-types';
 
 export const defaultTheme: AppThemeConfiguration = {
     app: {
-        'content-max-width': '1600px',
+        'content-max-width': '1800px',
         'overlay-header':
             'linear-gradient(transparent 0%, rgb(0 0 0 / 85%) 100%), var(--theme-background-noise)',
         'overlay-subheader':
