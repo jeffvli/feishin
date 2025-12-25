409c9e140959e001b0acc68836d380a78c3a52e6
jeffvli <jeffvictorli@gmail.com>
2025-12-24 17:41:37 -0800

    lint

--- src/renderer/features/visualizer/components/audiomotionanalyzer/visualizer-settings-modal.tsx
+++ src/renderer/features/visualizer/components/audiomotionanalyzer/visualizer-settings-modal.tsx
@@ -1,6 +1,4 @@
 import { VisualizerSettingsForm } from './visualizer-settings-form';
-import { TextTitle } from '/@/shared/components/text-title/text-title';
-import { Stack } from '/@/shared/components/stack/stack';
 
 export const VisualizerSettingsContextModal = () => {
     return <VisualizerSettingsForm />;
--- src/renderer/router/app-router.tsx
+++ src/renderer/router/app-router.tsx
@@ -2,13 +2,13 @@ import { lazy, Suspense } from 'react';
 import { HashRouter, Route, Routes } from 'react-router';
 
 import { ShuffleAllContextModal } from '/@/renderer/features/player/components/shuffle-all-modal';
-import { VisualizerSettingsContextModal } from '../features/visualizer/components/audiomotionanalyzer/visualizer-settings-modal';
 import { AddToPlaylistContextModal } from '/@/renderer/features/playlists/components/add-to-playlist-context-modal';
 import { SaveAndReplaceContextModal } from '/@/renderer/features/playlists/components/save-and-replace-context-modal';
 import { UpdatePlaylistContextModal } from '/@/renderer/features/playlists/components/update-playlist-form';
 import { SettingsContextModal } from '/@/renderer/features/settings/components/settings-modal';
 import { RouterErrorBoundary } from '/@/renderer/features/shared/components/router-error-boundary';
 import { ShareItemContextModal } from '/@/renderer/features/sharing/components/share-item-context-modal';
+import { VisualizerSettingsContextModal } from '/@/renderer/features/visualizer/components/audiomotionanalyzer/visualizer-settings-modal';
 import { AuthenticationOutlet } from '/@/renderer/layouts/authentication-outlet';
 import { ResponsiveLayout } from '/@/renderer/layouts/responsive-layout';
 import { AppOutlet } from '/@/renderer/router/app-outlet';
