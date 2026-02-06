36b698cc25ddf619f38f7ca96492b81561884667
Martín González Gómez <m@martingonzalez.net>
2026-02-05 19:08:59 -0600

    Open settings with shortcut. Also add settings to menubar.

--- src/preload/mpv-player.ts
+++ src/preload/mpv-player.ts
@@ -174,6 +174,10 @@ const rendererPlayerFallback = (cb: (event: IpcRendererEvent, data: boolean) => 
     ipcRenderer.on('renderer-player-fallback', cb);
 };
 
+const rendererOpenSettings = (cb: (event: IpcRendererEvent) => void) => {
+    ipcRenderer.on('renderer-open-settings', cb);
+};
+
 export const mpvPlayer = {
     autoNext,
     cleanup,
@@ -206,6 +210,7 @@ export const mpvPlayerListener = {
     rendererCurrentTime,
     rendererError,
     rendererNext,
+    rendererOpenSettings,
     rendererPause,
     rendererPlay,
     rendererPlayerFallback,
