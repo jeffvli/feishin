62aff76fc45c77810aac2a280d368894f93b437a
Villhume <65381730+Villhume@users.noreply.github.com>
2026-04-08 19:20:05 +1000

    Memory optimisation for CI attempt 6

--- vite.react-plugin.ts
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react';
-
-export function createReactPlugin() {
-    return react({
-        babel: {
-            plugins: ['babel-plugin-react-compiler'],
-        },
-    });
-}
